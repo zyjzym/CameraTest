a75451b524e6032b8b2d1ab577ac0d74dc7e5064
zhouyanjun <zhouyanjun@corsee.com>
2015-10-15 09:52:28 +0800

    新建文件main.c,增加源代码

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int
+main(int argc, char *argv[])
+{
+	char mem[1034] = {0};
+	while(1)
+	{
+	  memcpy(mem, "hello my friend", 100); 
+	  printf("%s", mem);
+	  
+
+	  printf("zhouyanjun:周延军\n"); 
+	  printf("zhouyanjun:周延军\n"); 
+	  printf("zhouyanjun:周延军\n"); 
+	  printf("zhouyanjun:周延军\n"); 
+	  printf("zhouyanjun:周延军\n"); 
+	  printf("zhouyanjun:周延军\n"); 
+	  printf("zhouyanjun:周延军\n"); 
+	 
+		//printf("goodbye my friend\n");
+	}
+	return 0;
+}
